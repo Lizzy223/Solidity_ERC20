1d9e29f758acf1756c0100b8b5203910b68aea7f
Elizabeth Kolade <54336149+Lizzy223@users.noreply.github.com>
2024-08-27 16:07:03 +0100

    update: made update

---
+++ LizzieToken.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+interface LizzieERC20 {
+     function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool);
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount)
+        external
+        returns (bool);
+}
+
+contract LizzieToken {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approve( address indexed  owner, address indexed  spender, uint256 value);
+
+    uint256 public totalSupply;
+    mapping (address =>uint256) public balanceOf;
+    mapping (address => mapping(address => uint256)) public allowance;
+
+    string public tokenName;
+    string public tokenSymbol;
+    uint8 public decimals;
+
+    constructor(string memory _tokenName, string memory _tokenSymbol, uint8 _decimals){
+        tokenName = _tokenName;
+        tokenSymbol = _tokenSymbol;
+        decimals = _decimals;
+    }
+
+    function transfer(address recipient, uint256 amount) external returns (bool){
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool){
+        allowance[msg.sender][spender] = amount;
+        emit Approve(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool){
+        allowance[msg.sender][sender] -= amount;
+        balanceOf[sender] -= amount;
+        balanceOf[recipient] -= amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    
+}

18053dfba8ca6bc0732d2092dc14a4e391c978ba
Elizabeth Kolade <54336149+Lizzy223@users.noreply.github.com>
2024-08-28 10:15:15 +0100

    Update myToken.sol

--- myToken.sol
+++ myToken.sol
@@ -1,3 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import "./LizzieToken.sol";
+
+contract MyToken is LizzieToken {
+    constructor()  LizzieToken("lizzietoken", "lt", 5) {
+        _mint(msg.sender, 100 * 10 ** uint256(decimals));
+    }
+
+}
 
 
 
